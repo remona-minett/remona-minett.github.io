cb04f510480ee588168a1c8250196535853914ba
Rem <50187357+remona-minett@users.noreply.github.com>
2020-03-29 10:21:32 -0700

    Update quotelines.js

--- js/quotelines.js
+++ js/quotelines.js
@@ -11,7 +11,18 @@ var lines = [
 'Loneliness is not fun. (10)',
 'I will tell you about myself someday. (11)',
 'Close your eyes, and you\'ll leave this place. (12)',
-'Goodnight. (13)'
+'Goodnight. (13)',
+'Everything that lives is designed to end. (14)',
+'The more of a fool people take you for, the more you\'ll learn of their true nature. (15)',
+'A future is not given to you. It is something you must take for yourself. (16)',
+'The time I was able to spend with you, it was like memories of pur light... (17)',
+'It always ends like this. (18)',
+'I\'m not quite sure what it means to mourn, or if we even have a soul to concern ourselves with. (19)',
+'Why do I desire the touch of something that no longer exists? (20)',
+'Being alive is pretty much a constant stream of embarassment. (21)',
+'Life is all about the struggle within the never ending spiral of life and death. (22)',
+'It doesn\'t matter. None of this matters. Isn\'t that hilarious? Doesn\'t it make you laugh? (23)',
+'Even if it\'s pointless, we still have to do it. (24)',
 ]
 
 function genLine() {
